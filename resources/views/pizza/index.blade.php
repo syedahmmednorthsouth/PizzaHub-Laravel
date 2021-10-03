49fedc3dcbd882f228dbeb26fb23356b9e688634
bob <ranjitkarki2@gmail.com>
2021-10-03 13:47:56 +1100

    laravel app

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -18,7 +18,7 @@
 
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Pizza</div>
+                <div class="card-header">Pizzas</div>
 
                 <div class="card-body">
                     <div class="form-group">
