15801ff1795d2369a3ea05587c2f0765402a5b32
bob <ranjitkarki2@gmail.com>
2021-10-04 12:25:01 +1100

    pizza seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Pizza::create([
+            'name' => 'plain cheese',
+            'description' => 'Tomato base, mozzarella and oreganoTomato base, mozzarella and oregano',
+            'small_pizza_price'=> 10,
+            'medium_pizza_price'=> 11,
+            'large_pizza_price'=> 12,
+            'category' => 'Non vegetaraian',
+            'image' => 'public/pizza/gCkP7K9ZpeflRlvPIEXIy6uZbI3yMBum1FYnYRtv.jpg',
+        ]);
+        Pizza::create([
+            'name' => 'plain cheese12',
+            'description' => 'Tomato base, mozzarella and oreganoTomato base, mozzarella and oregano',
+            'small_pizza_price'=> 11,
+            'medium_pizza_price'=> 12,
+            'large_pizza_price'=> 14,
+            'category' => 'Non vegetaraian',
+            'image' => 'public/pizza/9OVu8mYfQJt4zuxSvsK0Kpqcc5de8KieUyLbHCA0.jpg',
+        ]);
     }
 }
